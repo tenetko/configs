a48962b54df134166b8e224b3bba391b65761030
Michael Tenetko <tenetko@users.noreply.github.com>
2026-02-15 02:39:46 +0300

    (feat) nvim debugger configuration changes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,30 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- F5: Continue (Start / Resume)
+map("n", "<F5>", function()
+  require("dap").continue()
+end, { desc = "Debug: Continue" })
+
+-- F8: Step Over (Next line)
+map("n", "<F8>", function()
+  require("dap").step_over()
+end, { desc = "Debug: Step Over" })
+
+-- F7: Step Into (Inside function)
+map("n", "<F7>", function()
+  require("dap").step_into()
+end, { desc = "Debug: Step Into" })
+
+-- F12: Step Out (Return from function)
+map("n", "<F12>", function()
+  require("dap").step_out()
+end, { desc = "Debug: Step Out" })
+
+-- Leader+db: Toggle Breakpoint (Keep this, it's handy)
+map("n", "<leader>db", function()
+  require("dap").toggle_breakpoint()
+end, { desc = "Debug: Toggle Breakpoint" })
