b82f2d26922489408f3198948957d5494e3a8098
Michael Tenetko <tenetko@users.noreply.github.com>
2026-02-22 16:54:03 +0300

    (feat) added open lid config, renamed files

--- ansible/run_playbooks.sh
+++ ansible/run_playbooks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-TERM=dumb ANSIBLE_FORCE_COLOR=true ansible-playbook -u $USER -k -K install_and_configure_all.yaml
+TERM=dumb ANSIBLE_FORCE_COLOR=true ansible-playbook -u $USER -k -K all.yaml
